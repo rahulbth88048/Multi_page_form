3aaab1398a137a915e5d1733f58311ac4868bf15
Rahul Kumar <rahulbth88048@gmail.com>
2022-09-10 12:53:53 +0530

    update new

--- resources/views/product_list.blade.php
+++ resources/views/product_list.blade.php
@@ -7,26 +7,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Product List</title>
     <style>
-        .card{
+        .containers{
+            margin: 0px auto;
+            max-width: 1300px;
             display: flex;
+            justify-content: space-around;
             flex-wrap: wrap;
+            background-color: rgb(244, 240, 240);
+        }
+        img{
+            height: 15rem;
+        }
+        .card{
+            height: 30rem;
+            border-radius: 10px;
+            box-shadow: 5px 10px rgb(139, 137, 137);
+        }
+        .text-dark{
+            font-weight: bold;
         }
     </style>
 </head>
 <body>
-    <div class="container">
+    <div class="containers">
         @foreach($data as $item)
-        <div class="card" style="width: 30%; margin:10px auto;">
-        
-            <img src="{{$item['image']}}" style="height:14rem;"class="card-img-top" alt="...">
+        <div class="card m-4" style="width: 18rem;">
+            <img src="{{$item['image']}}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">{{$item['title']}}</h5>
-                <p class="card-text">{{$item['description']}}</p>
-                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+              <h5 class="card-title">{{$item['title']}}</h5>
+              <h6>{{$item['category']}}</h6>
+              <h6>${{$item['price']}}</h6>
+              <!-- <p class="card-text">{{$item['description']}}</p> -->
+              <div class="d-grid gap-2">
+                <a href="#" class="btn btn-warning text-dark">BUY NOW</a>
             </div>
-        
-               
-        </div>
+            </div>
+          </div>
         @endforeach
     </div>
        
